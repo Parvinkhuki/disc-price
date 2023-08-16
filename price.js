40fe7eb76895e2861f4da054dc37745e083cbc48
Parvinkhuki <khuki.developer@gmail.com>
2023-08-17 01:11:43 +0600

    done

---
+++ price.js
@@ -0,0 +1,35 @@
+const input = document.getElementById("input")
+const btn = document.getElementById("btn")
+const parag = document.getElementById("para")
+const paragg = document.getElementById("paraa")
+const btnn = document.getElementById("btnn")
+
+
+function elementId(element){
+    const inputs=document.getElementById(element)
+    const inputStrig = inputs.value 
+    const inputValue=parseFloat(inputStrig)
+    inputs.value ='';
+    return inputValue;
+}
+
+btn.addEventListener("click", function(){
+   const inputValue= elementId("input")
+    let newValue =(inputValue-((inputValue*30)/100));
+    parag.innerText= newValue;
+    return parag.innerText
+})
+btnn.addEventListener("click", function(){
+    const inputValue= elementId("input-s")
+    const discValue=document.getElementById("disk").value;
+    discValue="";
+    if(discValue==="Disc 30%"){
+     let newValue =(inputValue-((inputValue*30)/100));
+     paragg.innerText= newValue;
+     return paragg.innerText;
+    }
+    else{
+        paragg.innerText= inputValue;
+        return paragg.innerText
+ }})
+ 
